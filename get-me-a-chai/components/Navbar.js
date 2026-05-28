ca2da6257446faaf61e4e87cf6d5ab8895b7f296
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-05-28 22:38:45 +0530

    Added Home Page,Navbar and Footer

--- get-me-a-chai/components/Navbar.js
+++ get-me-a-chai/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react'
-
+import Link from 'next/link';
 function Navbar() {
   return (
     <nav className='bg-gray-900 text-white flex justify-between items-center px-4 h-16'>
-      <div className='logo font-bold text-lg'>
-        Get Me A Chai
+      <div className='logo font-bold text-lg flex items-center justify-center'>
+        <img src="/tea.gif"  width={40} alt="" />
+        <span>Get Me A Chai</span>
       </div>
-      <ul className='flex gap-4 justify-between'>
-        <li className=' cursor-pointer'>Home</li>
-        <li className=' cursor-pointer'>About</li>
-        <li className=' cursor-pointer'>Projects</li>
-        <li className=' cursor-pointer'>Sign In</li>
-        <li className=' cursor-pointer'>Login</li>
-      </ul>
+     
+<div>
+  <Link href={"/login"}>
+    <button
+      type="button"
+      className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+    >
+      Login
+    </button>
+  </Link>
+</div>
+
+
     </nav>
   )
 }
