16c2925fb4ad7e1a92c2039ea5875455547b4eb3
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-06-07 23:52:09 +0530

    Add About Page

--- app/login/page.js
+++ app/login/page.js
@@ -15,7 +15,7 @@ function Login() {
     <div className="text-white py-14 container mx-auto">
       <h1 className="text-center font-bold text-3xl">Login to Get Started</h1>
       <div className="flex flex-col gap-2 min-h-screen items-center  p-10">
-
+{/* 
         <button
         className="flex items-center w-64 bg-slate-50 text-black border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
           <svg
@@ -98,7 +98,7 @@ function Login() {
           </svg>
 
           <span>Continue with Facebook</span>
-        </button>
+        </button> */}
 
         <button
       onClick={() => {
